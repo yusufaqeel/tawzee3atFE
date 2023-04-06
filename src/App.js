73c84d984191fd550247754ab5fb4c69b417c447
Skhalfan <123301102+Skhalfan@users.noreply.github.com>
2023-04-06 16:48:45 +0300

    footer update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Axios from "axios";
 import jwt_decode from "jwt-decode";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from "./components/NavbarComp";
 import Footer from "./components/Footer";
-import Modal from "react-bootstrap/Modal";
-import './App.css'
+import "./App.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App() {
   return (
     <div className="App pageContainer">
       <NavbarComp />
-      <main className="mainS"> </main>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,138 +2,245 @@ import React from "react";
 import {
   MDBFooter,
   MDBContainer,
-  MDBRow,
-  MDBCol,
   MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
 } from "mdb-react-ui-kit";
 
-export default function Footer() {
+export default function App() {
   return (
     <div className="footer">
-    <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
-      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <div className="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
-        </div>
+      <MDBFooter className="text-center" color="white" bgColor="dark">
+        <MDBContainer className="p-4">
+          <section className="mb-4">
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab icon="facebook-f" />
+            </MDBBtn>
 
-        <div>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon color="secondary" fab icon="facebook-f" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon color="secondary" fab icon="twitter" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon color="secondary" fab icon="google" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon color="secondary" fab icon="instagram" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon color="secondary" fab icon="linkedin" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon color="secondary" fab icon="github" />
-          </a>
-        </div>
-      </section>
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab icon="twitter" />
+            </MDBBtn>
 
-      <section className="">
-        <MDBContainer className="text-center text-md-start mt-5">
-          <MDBRow className="mt-3">
-            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">
-                <MDBIcon color="secondary" icon="gem" className="me-3" />
-                Company name
-              </h6>
-              <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
-              </p>
-            </MDBCol>
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab icon="google" />
+            </MDBBtn>
 
-            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Angular
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  React
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab icon="instagram" />
+            </MDBBtn>
 
-            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Help
-                </a>
-              </p>
-            </MDBCol>
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab icon="linkedin-in" />
+            </MDBBtn>
 
-            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-              <p>
-                <MDBIcon color="secondary" icon="home" className="me-2" />
-                New York, NY 10012, US
-              </p>
-              <p>
-                <MDBIcon color="secondary" icon="envelope" className="me-3" />
-                info@example.com
-              </p>
-              <p>
-                <MDBIcon color="secondary" icon="phone" className="me-3" /> + 01
-                234 567 88
-              </p>
-              <p>
-                <MDBIcon color="secondary" icon="print" className="me-3" /> + 01
-                234 567 89
-              </p>
-            </MDBCol>
-          </MDBRow>
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab icon="github" />
+            </MDBBtn>
+          </section>
+
+          <section className="">
+            <form action="">
+              <MDBRow className="d-flex justify-content-center">
+                <MDBCol size="auto">
+                  <p className="pt-2">
+                    <strong>Sign up for our newsletter</strong>
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="5" start>
+                  <MDBInput
+                    contrast
+                    type="email"
+                    label="Email address"
+                    className="mb-4"
+                  />
+                </MDBCol>
+
+                <MDBCol size="auto">
+                  <MDBBtn outline color="light" type="submit" className="mb-4">
+                    Subscribe
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </form>
+          </section>
+
+          <section className="mb-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              distinctio earum repellat quaerat voluptatibus placeat nam,
+              commodi optio pariatur est quia magnam eum harum corrupti dicta,
+              aliquam sequi voluptate quas.
+            </p>
+          </section>
+
+          <section className="">
+            <MDBRow>
+              <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+                <h5 className="text-uppercase">Links</h5>
+
+                <ul className="list-unstyled mb-0">
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </MDBCol>
+
+              <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+                <h5 className="text-uppercase">Links</h5>
+
+                <ul className="list-unstyled mb-0">
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </MDBCol>
+
+              <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+                <h5 className="text-uppercase">Links</h5>
+
+                <ul className="list-unstyled mb-0">
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </MDBCol>
+
+              <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+                <h5 className="text-uppercase">Links</h5>
+
+                <ul className="list-unstyled mb-0">
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </MDBCol>
+            </MDBRow>
+          </section>
         </MDBContainer>
-      </section>
 
-      <div
-        className="text-center p-4"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-      >
-        © 2021 Copyright:
-        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-          MDBootstrap.com
-        </a>
-      </div>
-    </MDBFooter>
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2020 Copyright:
+          <a className="text-white" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+      </MDBFooter>
     </div>
   );
 }
