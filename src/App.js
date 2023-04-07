c9daf35c7e6e35e3a195f9e286bbcdb8708c9451
Yusuf Aqeel <y.aqeel@hotmail.com>
2023-04-07 16:51:48 +0300

    adding routes in the nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-// import Signup from "./user/Signup";
-// import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Axios from "axios";
 import jwt_decode from "jwt-decode";
@@ -11,6 +11,61 @@ import Modal from "react-bootstrap/Modal";
 import './App.css'
 
 export default function App() {
+
+  const [isAuth, setIsAuth] = useState(false)
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    let token = localStorage.getItem("token")
+    if(token != null){
+      let user = jwt_decode(token)
+
+      if(user){
+        setIsAuth(true)
+        setUser(user)
+      }
+      else if (!user){
+        localStorage.removeItem("token")
+        setIsAuth(false)
+      }
+    }
+  }, [])
+
+  const registerHandler = (user) => {
+    Axios.post("auth/signup", user)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const loginHandler = (cred) => {
+    Axios.post("auth/signin", cred)
+    .then(res => {
+      console.log(res.data.token)
+      // Save the token into Local Storage
+      let token = res.data.token
+      if(token != null){
+        localStorage.setItem("token", token)
+        let user = jwt_decode(token);
+        setIsAuth(true)
+        setUser(user)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      setIsAuth(false)
+    })
+  }
+
+  const onLogoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token")
+    setIsAuth(false)
+    setUser(null)
+  }
   return (
     <div className="App pageContainer">
       <NavbarComp />
--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,4 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import Signup from '../user/Signup'
+import Signin from '../user/Signin'
+import ItemList from '../item/ItemList'
+import Axios from "axios";
+import jwt_decode from "jwt-decode";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import {
   Navbar,
   Nav,
@@ -9,21 +15,97 @@ import {
   Container,
 } from "react-bootstrap";
 
-export default class NavbarComp extends Component {
-  render() {
-    return (
-      <div>
+export default function NavbarComp() {
+
+  const [isAuth, setIsAuth] = useState(false)
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    let token = localStorage.getItem("token")
+    if(token != null){
+      let user = jwt_decode(token)
+
+      if(user){
+        setIsAuth(true)
+        setUser(user)
+      }
+      else if (!user){
+        localStorage.removeItem("token")
+        setIsAuth(false)
+      }
+    }
+  }, [])
+
+  const registerHandler = (user) => {
+    Axios.post("auth/signup", user)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const loginHandler = (cred) => {
+    Axios.post("auth/signin", cred)
+    .then(res => {
+      console.log(res.data.token)
+      // Save the token into Local Storage
+      let token = res.data.token
+      if(token != null){
+        localStorage.setItem("token", token)
+        let user = jwt_decode(token);
+        setIsAuth(true)
+        setUser(user)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      setIsAuth(false)
+    })
+  }
+
+  const onLogoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token")
+    setIsAuth(false)
+    setUser(null)
+  }
+
+  return (
+    <>
+    <Router>
+    <div>
         <Navbar bg="dark" variant="dark">
           <Container>
             <Navbar.Brand href="#home">Tawzee3at</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Signin</Nav.Link>
-              <Nav.Link href="#pricing">Signup</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/signin">Signin</Nav.Link>
+              <Nav.Link href="/signup">Signup</Nav.Link>
+              <Nav.Link href="/logout" onClick={onLogoutHandler}>Logout</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
       </div>
-    );
+      <div>
+        <Routes>
+        <Route path="/" element={
+                isAuth ? 
+                <ItemList /> 
+                : 
+                <Signin login={loginHandler}></Signin>}>
+        </Route>
+        <Route path="/signup" element={<Signup register={registerHandler} />}></Route>
+        <Route path="/signin" element={
+                isAuth ? 
+                <ItemList /> 
+                : 
+                <Signin login={loginHandler}></Signin>}>
+        </Route>
+        </Routes>
+      </div>
+      </Router>
+      </>
+  )
   }
-}
