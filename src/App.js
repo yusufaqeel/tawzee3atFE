644dff8eee90f84ed299cbd7b826a78d5627f6d8
Skhalfan <123301102+Skhalfan@users.noreply.github.com>
2023-04-11 11:21:57 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState, useEffect } from "react";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
-import ItemList from './item/ItemList'
+import ItemList from "./item/ItemList";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Axios from "axios";
 import jwt_decode from "jwt-decode";
@@ -27,118 +27,120 @@ export default function App() {
   const [showNavColorSecond, setShowNavColorSecond] = useState(false);
   const [showNavColorThird, setShowNavColorThird] = useState(false);
 
-
-  const [isAuth, setIsAuth] = useState(false)
+  const [isAuth, setIsAuth] = useState(false);
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    let token = localStorage.getItem("token")
-    if(token != null){
-      let user = jwt_decode(token)
+    let token = localStorage.getItem("token");
+    if (token != null) {
+      let user = jwt_decode(token);
 
-      if(user){
-        setIsAuth(true)
-        setUser(user)
-      }
-      else if (!user){
-        localStorage.removeItem("token")
-        setIsAuth(false)
+      if (user) {
+        setIsAuth(true);
+        setUser(user);
+      } else if (!user) {
+        localStorage.removeItem("token");
+        setIsAuth(false);
       }
     }
-  }, [])
+  }, []);
 
   const registerHandler = (user) => {
     Axios.post("auth/signup", user)
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const loginHandler = (cred) => {
     Axios.post("auth/signin", cred)
-    .then(res => {
-      console.log(res.data.token)
-      // Save the token into Local Storage
-      let token = res.data.token
-      if(token != null){
-        localStorage.setItem("token", token)
-        let user = jwt_decode(token);
-        setIsAuth(true)
-        setUser(user)
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      setIsAuth(false)
-    })
-  }
+      .then((res) => {
+        console.log(res.data.token);
+        // Save the token into Local Storage
+        let token = res.data.token;
+        if (token != null) {
+          localStorage.setItem("token", token);
+          let user = jwt_decode(token);
+          setIsAuth(true);
+          setUser(user);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsAuth(false);
+      });
+  };
 
   const onLogoutHandler = (e) => {
     e.preventDefault();
-    localStorage.removeItem("token")
-    setIsAuth(false)
-    setUser(null)
-  }
+    localStorage.removeItem("token");
+    setIsAuth(false);
+    setUser(null);
+  };
 
   return (
     <>
-    <Router>
-    <div>
-      <MDBNavbar expand="lg" dark bgColor="dark">
-        <MDBContainer fluid>
-          <MDBNavbarBrand href="/">Tawzee3at</MDBNavbarBrand>
-          <MDBNavbarToggler
-            type="button"
-            data-target="#navbarColor02"
-            aria-controls="navbarColor02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => setShowNavColorSecond(!showNavColorSecond)}
-          >
-            <MDBIcon icon="bars" fas />
-          </MDBNavbarToggler>
-          <MDBCollapse show={showNavColorSecond} navbar id="navbarColor02">
-            <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
-              <MDBNavbarItem className="active">
-                <MDBNavbarLink aria-current="page" href="/">
-                  Home
-                </MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/item">Shop</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/signin">Signin</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/signup">Signup</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/logout"onClick={onLogoutHandler}>Signout</MDBNavbarLink>
-              </MDBNavbarItem>
-            </MDBNavbarNav>
-          </MDBCollapse>
-        </MDBContainer>
-      </MDBNavbar>
-    </div>
-    <div>
-      <Routes>
-        <Route path="/"></Route>
-        <Route path="/signup" element={<Signup register={registerHandler} />}></Route>
-        <Route path="/signin" element={
-          isAuth ?
-          <ItemList />
-          :
-          <Signin login={loginHandler}></Signin>
-        }></Route>
-      </Routes>
-    </div>
-      <Footer />
+      <Router>
+        <div>
+          <MDBNavbar expand="lg" dark bgColor="dark">
+            <MDBContainer fluid>
+              <MDBNavbarBrand href="/">Tawzee3at</MDBNavbarBrand>
+              <MDBNavbarToggler
+                type="button"
+                data-target="#navbarColor02"
+                aria-controls="navbarColor02"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={() => setShowNavColorSecond(!showNavColorSecond)}
+              >
+                <MDBIcon icon="bars" fas />
+              </MDBNavbarToggler>
+              <MDBCollapse show={showNavColorSecond} navbar id="navbarColor02">
+                <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
+                  <MDBNavbarItem>
+                    <MDBNavbarLink aria-current="page" href="/">
+                      Home
+                    </MDBNavbarLink>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink href="/item">Shop</MDBNavbarLink>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink href="/signin">Signin</MDBNavbarLink>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink href="/signup">Signup</MDBNavbarLink>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink href="/logout" onClick={onLogoutHandler}>
+                      Signout
+                    </MDBNavbarLink>
+                  </MDBNavbarItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+        </div>
+        <div>
+          <Routes>
+            <Route path="/"></Route>
+            <Route
+              path="/signup"
+              element={<Signup register={registerHandler} />}
+            ></Route>
+            <Route
+              path="/signin"
+              element={
+                isAuth ? <ItemList /> : <Signin login={loginHandler}></Signin>
+              }
+            ></Route>
+          </Routes>
+        </div>
+        <Footer />
       </Router>
-      </>
-  )
-  }
-
+    </>
+  );
+}
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,52 +1,53 @@
-import React, { useState } from 'react'
-import {Container, Form, Button} from "react-bootstrap"
+import React, { useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
 export default function Signup(props) {
+  const [newUser, setNewUser] = useState({});
 
-    const [newUser, setNewUser] = useState({});
+  const ChangeHandler = (e) => {
+    const user = { ...newUser };
+    user[e.target.name] = e.target.value;
+    setNewUser(user);
+    console.log(user);
+  };
 
-    const ChangeHandler = (e) => {
-        const user = {...newUser}
-        user[e.target.name] = e.target.value
-        setNewUser(user)
-        console.log(user)
-    }
-
-    const registerHandler = () => {
-        props.register(newUser)
-    }
+  const registerHandler = () => {
+    props.register(newUser);
+  };
 
   return (
     <div>
-        <h1>Sign Up</h1>
+      <h1 className="title">Sign Up</h1>
 
-        <Container>
-            <Form.Group>
-                <Form.Label>First Name</Form.Label>
-                <Form.Control name="firstName" onChange={ChangeHandler}/>
-            </Form.Group>
+      <Container className="form">
+        <Form.Group>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control name="firstName" onChange={ChangeHandler} />
+        </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control name="lastName" onChange={ChangeHandler} />
-            </Form.Group>
+        <Form.Group>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control name="lastName" onChange={ChangeHandler} />
+        </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control name="emailAddress" onChange={ChangeHandler} />
-            </Form.Group>
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control name="emailAddress" onChange={ChangeHandler} />
+        </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control name="password" type="password" onChange={ChangeHandler} />
-            </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            type="password"
+            onChange={ChangeHandler}
+          />
+        </Form.Group>
 
-            <Button variant="primary" onClick={registerHandler}>
-                Register
-            </Button>
-
-        </Container>
-        
+        <Button variant="grey" onClick={registerHandler}>
+          Register
+        </Button>
+      </Container>
     </div>
-  )
+  );
 }
